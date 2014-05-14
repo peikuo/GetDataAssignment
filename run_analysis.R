76f09907f3e1e19a235e1f9a5b107b6a6071dab5
peikuo <peikuopeikuo@gmail.com>
2014-05-14 09:32:44 +0800

    assignment v1

---
+++ run_analysis.R
@@ -0,0 +1,40 @@
+library(reshape2)
+
+mergeAndExtractData <- function( directory ) {
+  
+  feathers <- read.table(paste0(directory, "/features.txt"))
+  useful_feathers <- feathers[grep("mean\\(\\)|std\\(\\)", feathers[,2]), ]
+  
+  train_set <- read.table(paste0(directory, "/train/X_train.txt"))
+  test_set <- read.table(paste0(directory, "/test/X_test.txt"))
+  all_set <- rbind(train_set[, useful_feathers[, 1]], test_set[, useful_feathers[, 1]])
+  colnames(all_set) <- useful_feathers[, 2]
+  
+  train_activity <- read.table(paste0(directory, "/train/y_train.txt"), col.names=c("activity_id"))
+  test_activity <- read.table(paste0(directory, "/test/y_test.txt"), col.names=c("activity_id"))
+  all_activity <- rbind(train_activity, test_activity)
+  activity_label <- read.table(paste0(directory, "/activity_labels.txt"), col.names=c("activity_id", "activity_label"))
+  
+  train_subject <- read.table(paste0(directory, "/train/subject_train.txt"), col.names="subject")
+  test_subject <- read.table(paste0(directory, "/test/subject_test.txt"), col.names="subject")
+  all_subject <- rbind(train_subject, test_subject)
+  
+  all_data <- cbind(all_set, all_activity, all_subject)
+  all_data <- merge(all_data, activity_label, by.x="activity_id", by.y="activity_id", all.x=TRUE, sort=FALSE)
+  
+  all_data
+}
+
+getTidyData <- function(data) {
+  groupData <- melt(data, id.vars = c("subject", "activity_id", "activity_label"))
+  groupData$value <- as.numeric(groupData$value)
+  meanData <- aggregate(value~subject+activity_id+activity_label+variable, data=groupData, FUN=mean)
+  tidyData <- dcast(meanData, subject+activity_id+activity_label ~ variable)
+  tidyData
+}
+
+writeTidyData <- function(data, filename) {
+  write.table(data, file=filename, quote=FALSE, sep = " ", row.names=FALSE)
+}
+
+
